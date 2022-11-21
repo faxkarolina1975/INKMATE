59382fb95d539ebae3cbb98c83dd7954efb80125
Chairessimental <urieliner@gmail.com>
2022-11-20 18:00:10 -0600

    Login y Registro

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,26 @@
+import React, {useEffect, useState} from "react"
 
 function Login(){
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function login(email, password){
+
+        const user = {email, password};
+        const response = await fetch('http://localhost:8080/api/auth/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(user)
+        } )
+        .then(r=>r.json())
+
+        console.log(response)
+    }
+
+
+
+
     return(
         <div>
 
@@ -10,30 +31,45 @@ function Login(){
                     <div  className="form-items">
                         <h3>Login</h3>
                         <p>Fill in the data below.</p>
-                        <form  className="requires-validation" novalidate/>
+                        <form onSubmit={ev =>{ ev.preventDefault(); login(email, password);}} className="requires-validation" noValidate>
                          
-                         
-
                            <div  className="col-md-12">
-                                <input  className="form-control" type="email" name="email" placeholder="E-mail Address"/>
+                                <input  
+                                className="form-control" 
+                                type="email" 
+                                name="email" 
+                                placeholder="E-mail Address"
+                                value={email}
+                                onChange={ev => setEmail(ev.target.value)}
+                                />
+
                                  {/* <div  className="valid-feedback">Email field is valid!</div>
                                  <div  className="invalid-feedback">Email field cannot be blank!</div> */}
                             </div>
 
-                          
-
-                           <div class="col-md-12">
-                              <input  className="form-control" type="password" name="password" placeholder="Password"/>
+                           <div className="col-md-12">
+                              <input  
+                              className="form-control" 
+                              type="password" 
+                              name="password" 
+                              placeholder="Password"
+                              value={password}
+                              onChange={ev => setPassword(ev.target.value)}
+                              />
                                {/* <div  className="valid-feedback">Password field is valid!</div>
                                <div  className="invalid-feedback">Password field cannot be blank!</div> */}
                            </div>
 
-                                     
-
-                            <div class="form-button mt-3" >
-                                <button id="submit" type="submit" class="btn btn-dark">Login</button>
+                            <div className="form-button mt-3" >
+                                <button 
+                                id="submit" 
+                                type="submit" 
+                                className="btn btn-dark"
+                                >Login
+                                </button>
                             </div>
-                    
+                            </form>
+
                     </div>
                 </div>
             </div>
--- src/Components/Registro.jsx
+++ src/Components/Registro.jsx
@@ -1,8 +1,35 @@
+import React, {useEffect, useState } from "react";
 
 function Registro(){
+ 
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function createUsers(name, lastname, username, password, email){
+
+        const birthday = '2022-02-01'
+
+        const newUser = {email, name, lastname, username, birthday, password};
+
+        const response = await fetch('http://localhost:8080/api/user', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newUser)
+        } )
+        .then(r=>r.json())
+
+        console.log(response)
+    }
+
+    
+
+
     return(
         <div>
-
+                        
         <div className="form-body">
         <div  className="row">
             <div  className="form-holder" style={{ backgroundColor: "#8f41c4"}}>
@@ -10,29 +37,48 @@ function Registro(){
                     <div  className="form-items">
                         <h3>Register Today</h3>
                         <p>Fill in the data below.</p>
-                        <form  className="requires-validation" novalidate/>
+                        <form onSubmit={ev =>{ 
+                            ev.preventDefault(); 
+                            createUsers(firstName, lastName, username, password, email);}} 
+                            className="requires-validation" noValidate>
                          
-                        <div  className="col-md-12">
-                                <input  className="form-control" type="text" name="text" placeholder="First Name"/>
+                            <div  className="col-md-12">
+                                <input  className="form-control" 
+                                type="text" 
+                                name="firstName" 
+                                placeholder="First Name"
+                                value={firstName}
+                                onChange={ev => setFirstName(ev.target.value)}
+                                />
                                  {/* <div  className="valid-feedback">Email field is valid!</div>
                                  <div  className="invalid-feedback">Email field cannot be blank!</div> */}
                             </div>
 
                             <div  className="col-md-12">
-                                <input  className="form-control" type="text" name="text" placeholder="Last Name"/>
+                                <input  className="form-control" 
+                                type="text" 
+                                name="lastName" 
+                                placeholder="Last Name"
+                                value={lastName}
+                                onChange={ev => setLastName(ev.target.value)}/>
                                  {/* <div  className="valid-feedback">Email field is valid!</div>
                                  <div  className="invalid-feedback">Email field cannot be blank!</div> */}
                             </div>
 
                             <div  className="col-md-12">
-                                <input  className="form-control" type="text" name="text" placeholder="User Name"/>
+                                <input  className="form-control" 
+                                type="text" 
+                                name="username" 
+                                placeholder="User Name"
+                                value={username}
+                                onChange={ev => setUsername(ev.target.value)}/>
                                  {/* <div  className="valid-feedback">Email field is valid!</div>
                                  <div  className="invalid-feedback">Email field cannot be blank!</div> */}
                             </div>
 
                             <div  className="col-md-12">
                                 <select  className="form-select mt-3">
-                                      <option selected disabled value="">Position</option>
+                                      <option defaultValue=''>Position</option>
                                       <option value="jweb">Tatuador</option>
                                       <option value="sweb">Cliente</option>
                                      
@@ -42,37 +88,56 @@ function Registro(){
                            </div>
 
 
-                           <div class="col-md-12">
-                              <input  className="form-control" type="password" name="password" placeholder="Password"/>
+                           <div className="col-md-12">
+                              <input  className="form-control" 
+                              type="password" 
+                              name="password" 
+                              placeholder="Password"
+                              value={password}
+                              onChange={ev => setPassword(ev.target.value)}/>
                                {/* <div  className="valid-feedback">Password field is valid!</div>
                                <div  className="invalid-feedback">Password field cannot be blank!</div> */}
                            </div>
 
-                           <div class="col-md-12">
-                              <input  className="form-control" type="password" name="password" placeholder="Confirm Password"/>
+                           <div className="col-md-12">
+                              <input  className="form-control" 
+                              type="password" 
+                              name="password2" 
+                              placeholder="Confirm Password"/>
                                {/* <div  className="valid-feedback">Password field is valid!</div>
                                <div  className="invalid-feedback">Password field cannot be blank!</div> */}
                            </div>
 
                            <div  className="col-md-12">
-                                <input  className="form-control" type="email" name="email" placeholder="E-mail Address"/>
+                                <input  className="form-control" 
+                                type="email" 
+                                name="email" 
+                                placeholder="E-mail Address"
+                                value={email}
+                                onChange={ev => setEmail(ev.target.value)}/>
                                  {/* <div  className="valid-feedback">Email field is valid!</div>
                                  <div  className="invalid-feedback">Email field cannot be blank!</div> */}
                             </div>             
                   
-
-                            <div class="form-button mt-3" >
-                                <button id="submit" type="submit" class="btn btn-dark">Register</button>
+                            <div className="form-button mt-3" >
+                                <button id="submit" 
+                                type="submit" 
+                                className="btn btn-dark">Register</button>
                             </div>
-                    
+                        </form>
+                            
+                        
+                       
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 
     </div>
     )
+
 }
 
 export default Registro
