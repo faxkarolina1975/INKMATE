132aa7b9e651c102d090c9f061c4c6a1275f65e9
JMPMorgan <jonathan.morganp@gmail.com>
2022-11-24 21:23:32 -0600

    Fix posts

--- src/Components/FeedComponents/Tarjeta2.jsx
+++ src/Components/FeedComponents/Tarjeta2.jsx
@@ -1,25 +1,31 @@
-function Tarjeta2({post}){
-  console.log(post)
-  return(
-    <div className="col">
-      
-          <div className="card shadow-sm" style={{backgroundColor: "#8f41c4"}}>
-            
-            <img className="bd-placeholder-img card-img-top" width="100%" height="100" src={post.img} ><title>Placeholder</title></img>
-
-            <div className="card-body" style={{backgroundColor: "#23001E"}}>
-              
-              <div className="d-flex justify-content-between align-items-center">
-                <small className="text" style={{color: "white"}}>{post.description}</small>
-                <button type="button" className="btn-close"aria-label="Close"></button>
-              </div>
+function Tarjeta2({ post }) {
+  console.log(post);
+  if (post) {
+    return (
+      <div className="col">
+        <div className="card shadow-sm" style={{ backgroundColor: "#8f41c4" }}>
+          <img
+            className="bd-placeholder-img card-img-top"
+            width="100%"
+            height="100"
+            src={post.img}
+            alt={post._id}
+          ></img>
+          <div className="card-body" style={{ backgroundColor: "#23001E" }}>
+            <div className="d-flex justify-content-between align-items-center">
+              <small className="text" style={{ color: "white" }}>
+                {post.description}
+              </small>
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+              ></button>
             </div>
           </div>
-          
-        
-
-          
-    </div>
-  )
+        </div>
+      </div>
+    );
+  }
 }
-export default Tarjeta2
+export default Tarjeta2;
